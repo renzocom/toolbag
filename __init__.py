27b2dc478be18529eafb5ef877ad82f88d44c64a
Renzo <renzo.com@gmail.com>
2019-12-13 16:55:27 +0100

    add read_write.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+""" Toolbag full of tools."""
+
+__author__ = "Renzo Comolatti"
+__email__ = "renzo.com@gmail.com"
+__license__ = "GPL"
---
+++ read_write.py
@@ -0,0 +1,87 @@
+import mne
+import numpy as np
+import scipy as sp
+import pandas as pd
+import pickle
+from itcfpy.spatial import make_bip_coords
+from itcfpy.read_write import loadmat
+import os
+import glob
+import scipy.io as sio
+import sys
+from pathlib import Path
+import numpy as np
+
+def get_dataset_info(dataset_dir, filename, n_levels=0, dir_pattern=None):
+    if not Path(dataset_dir).is_dir():
+        raise FileNotFoundError(dataset_dir)
+
+    data_info = []
+    data_path = []
+
+    if dir_pattern is None:
+        dir_pattern = n_levels * '**/'
+    pattern = os.path.join(dataset_dir,dir_pattern,filename)
+        
+    files = glob.glob(pattern)
+
+    for file in files:
+        s = file.split('/')
+        parents = s[-(n_levels+1):-1]
+        filename = s[-1].split('.')[:-1]
+        data_info.append(parents + filename)
+        data_path.append(file)
+
+    print(f'{len(data_info)} files found matching {pattern}.')
+    return np.array(data_path), np.array(data_info)
+
+def save_pickle(fname, variable):
+    if not fname.endswith('.pkl'):
+        fname += '.pkl'
+    output = open(fname, 'wb')
+    pickle.dump(variable, output)
+    output.close()
+
+
+def load_pickle(fname):
+    if not fname.endswith('.pkl'):
+        fname += '.pkl'
+    pkl_file = open(fname, 'rb')
+    variable = pickle.load(pkl_file)
+    pkl_file.close()
+    return variable
+
+def loadmat(filename):
+    """
+    this function should be called instead of direct spio.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects
+    """
+    data = spio.loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_keys(data)
+
+
+def _check_keys(dict_in):
+    """
+    checks if entries in dictionary are mat-objects. If yes
+    todict is called to change them to nested dictionaries
+    """
+    for key in dict_in:
+        if isinstance(dict_in[key], spio.matlab.mio5_params.mat_struct):
+            dict_in[key] = _todict(dict_in[key])
+    return dict_in
+
+
+def _todict(matobj):
+    """
+    A recursive function which constructs from matobjects nested dictionaries
+    """
+    dict = {}
+    for strg in matobj._fieldnames:
+        elem = matobj.__dict__[strg]
+        if isinstance(elem, spio.matlab.mio5_params.mat_struct):
+            dict[strg] = _todict(elem)
+        else:
+            dict[strg] = elem
+    return dict
